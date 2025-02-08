8a99d8d303faafac303e6fd1d56580698d6874fe
Mariya <mariyagracious07@gmail.com>
2025-02-09 01:39:05 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         while(true){
+            System.out.print("$ ");
         String input = scanner.nextLine();
         if(input.isEmpty()){
             continue;
