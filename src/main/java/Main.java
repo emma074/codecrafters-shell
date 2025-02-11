2b8d46a6227eb558682c7872870cdd2422be8c09
Mariya <mariyagracious07@gmail.com>
2025-02-12 01:22:30 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,33 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-<<<<<<< HEAD
-        System.out.print("$ ");
-
-=======
->>>>>>> 743c73c87856df9390b16a47e2eedea444bfb614
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while(true)
+        {
             System.out.print("$ ");
         String input = scanner.nextLine();
-<<<<<<< HEAD
-        System.out.println(input +": command not found");
-        System.out.flush();  
-}
-=======
-        if(input.isEmpty()){
+        if(input.isEmpty())
+        {
             continue;
         }
-        else if(input.equals("exit 0")){
+        else if(input.equals("exit 0"))
+        {
             System.exit(0);
         }
-        else{
+        else if(input.startsWith("echo"))
+        {
+            System.out.println(input.substring(5));
+        }
+        else
+        {
 
         System.out.println(input +": command not found");
         }
 
 
     }
->>>>>>> 743c73c87856df9390b16a47e2eedea444bfb614
 }
 }
+
