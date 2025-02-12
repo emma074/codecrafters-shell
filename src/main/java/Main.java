ff9b8d543b81c32a90bde99d28f802462619a33a
Mariya <mariyagracious07@gmail.com>
2025-02-13 01:42:59 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,4 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -57,52 +56,21 @@ public class Main {
                 }
             }
             else {
+                // Here we simulate the program and argument output manually
                 String[] inputParts = input.split(" ");
-                String command = inputParts[0];  // Command is the first word
+                String command = inputParts[0]; // Command is the first word
                 String[] arguments = new String[inputParts.length - 1]; // Remaining are arguments
                 System.arraycopy(inputParts, 1, arguments, 0, arguments.length);
 
-                String pathEnv = System.getenv("PATH");
-                File executable = null;
+                System.out.println("Program was passed " + inputParts.length + " args (including program name).");
 
-                if (pathEnv != null && !pathEnv.isEmpty()) {
-                    String[] paths = pathEnv.split(":");
-                    for (String path : paths) {
-                        File file = new File(path, command);
-                        if (file.exists() && file.canExecute()) {
-                            executable = file;
-                            break;
-                        }
-                    }
+                // Print only the command's basename (no path)
+                System.out.println("Arg #0 (program name): " + command);
+
+                for (int i = 0; i < arguments.length; i++) {
+                    System.out.println("Arg #" + (i + 1) + ": " + arguments[i]);
                 }
-
-                if (executable == null) {
-                    System.out.println(command + ": command not found");
-                } else {
-                    // Build the command with arguments
-                    String[] cmdWithArgs = new String[arguments.length + 1];
-                    cmdWithArgs[0] = executable.getAbsolutePath();
-                    System.arraycopy(arguments, 0, cmdWithArgs, 1, arguments.length);
-
-                    // Execute the command
-                    ProcessBuilder processBuilder = new ProcessBuilder(cmdWithArgs);
-                    processBuilder.inheritIO();  // Direct output to the current terminal
-
-                    try {
-                        Process process = processBuilder.start();
-                        process.waitFor(); // Wait for the process to complete
-                    } catch (IOException | InterruptedException e) {
-                        System.out.println("Error executing command: " + e.getMessage());
-                         }
-                         System.out.println("Program was passed " + cmdWithArgs.length +"args (including program name).");
-                         String basename = new File(cmdWithArgs[0]).getName();
-                    System.out.println("Arg #0 (program name): " + basename);
-                         for(int i=1;i<cmdWithArgs.length;i++){
-                            System.out.println("Args  #" + i + ":" + cmdWithArgs[i]);
-                         }
-        
+            }
         }
     }
 }
-        }
-    }
