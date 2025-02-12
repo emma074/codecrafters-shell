6e6f8f448eb5ec8e720265f6808332b1b55c6228
Mariya <mariyagracious07@gmail.com>
2025-02-13 01:51:55 +0530

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
@@ -55,20 +57,31 @@ public class Main {
                     }
                 }
             }
-            else {
-                // Here we simulate the program and argument output manually
-                String[] inputParts = input.split(" ");
-                String command = inputParts[0]; // Command is the first word
-                String[] arguments = new String[inputParts.length - 1]; // Remaining are arguments
-                System.arraycopy(inputParts, 1, arguments, 0, arguments.length);
+             else {
+                try {
+                    // Split input into command and arguments
+                    String[] inputParts = input.split(" ");
+                    ProcessBuilder processBuilder = new ProcessBuilder(inputParts);
+                    processBuilder.redirectErrorStream(true); // Combine stdout and stderr
 
-                System.out.println("Program was passed " + inputParts.length + " args (including program name).");
+                    // Start the process and capture output
+                    Process process = processBuilder.start();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    String line;
 
-                // Print only the command's basename (no path)
-                System.out.println("Arg #0 (program name): " + command);
+                    // Print each line of the process output
+                    while ((line = reader.readLine()) != null) {
+                        System.out.println(line);
+                    }
 
-                for (int i = 0; i < arguments.length; i++) {
-                    System.out.println("Arg #" + (i + 1) + ": " + arguments[i]);
+                    // Wait for the process to finish
+                    int exitCode = process.waitFor();
+                    if (exitCode != 0) {
+                        System.out.println("Command exited with code " + exitCode);
+                    }
+
+                } catch (Exception e) {
+                    System.out.println("Failed to execute command: " + e.getMessage());
                 }
             }
         }
